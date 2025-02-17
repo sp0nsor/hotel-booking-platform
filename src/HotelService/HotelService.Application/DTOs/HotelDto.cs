19293042d63291b996a6737c34602aea7911dc19
t3rixis <spons0rr1703@gmail.com>
2025-02-18 01:09:28 +0300

    feat: add DTOs

--- src/HotelService/HotelService.Application/DTOs/HotelDto.cs
+++ src/HotelService/HotelService.Application/DTOs/HotelDto.cs
@@ -1,4 +1,14 @@
 ﻿namespace HotelService.Application.DTOs
 {
-    public record HotelDto();
+    public record HotelDto(
+        Guid Id,
+        string Name,
+        string Description,
+        string PhoneNumber,
+        string Country,
+        string City,
+        string Street,
+        string Category,
+        string ImageUrl,
+        List<RoomDto> Rooms);
 }
---
+++ src/HotelService/HotelService.Application/DTOs/RoomDto.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelService.Application.DTOs
+{
+    public record RoomDto(
+        Guid Id,
+        Guid HotelId,
+        int Capacity,
+        int Area,
+        int Number,
+        int moneyAmount,
+        string Currency,
+        string ImageUrl,
+        List<BookedDatesDto> BookedDates);
+}
