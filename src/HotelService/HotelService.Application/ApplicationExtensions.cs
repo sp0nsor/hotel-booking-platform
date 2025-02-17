bdd7f803babb3134437c7e67aab1dba2b6e0b3bc
t3rixis <spons0rr1703@gmail.com>
2025-02-18 00:52:56 +0300

    chore: update project extensions

---
+++ src/HotelService/HotelService.Application/ApplicationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using HotelService.Application.Interfaces;
+using HotelService.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelService.Application
+{
+    public static class ApplicationExtensions
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            services.AddScoped<IImageService, ImageService>();
+
+            return services;
+        }
+    }
+}
